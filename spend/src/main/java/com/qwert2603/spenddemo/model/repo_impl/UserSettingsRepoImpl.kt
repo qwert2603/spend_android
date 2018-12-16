a8687c628390b52805022b08f66e1770ea3b8d9c
alex <qwert2603@mail.ru>
2018-12-16 18:03:24 +0700

    show sums & changeKinds by default in records list

--- spend/src/main/java/com/qwert2603/spenddemo/model/repo_impl/UserSettingsRepoImpl.kt
+++ spend/src/main/java/com/qwert2603/spenddemo/model/repo_impl/UserSettingsRepoImpl.kt
@@ -1,11 +1,7 @@
 package com.qwert2603.spenddemo.model.repo_impl
 
 import android.content.Context
-import com.google.gson.Gson
-import com.qwert2603.spenddemo.model.entity.SDate
-import com.qwert2603.spenddemo.model.entity.STime
 import com.qwert2603.spenddemo.model.repo.UserSettingsRepo
-import com.qwert2603.spenddemo.utils.PreferenceUtils
 import com.qwert2603.spenddemo.utils.PrefsBoolean
 import com.qwert2603.spenddemo.utils.PrefsInt
 import javax.inject.Inject
@@ -18,8 +14,8 @@ class UserSettingsRepoImpl @Inject constructor(appContext: Context) : UserSettin
 
     override var showSpends by PrefsBoolean(prefs, "showSpends", true)
     override var showProfits by PrefsBoolean(prefs, "showProfits", true)
-    override var showSums by PrefsBoolean(prefs, "showSums")
-    override var showChangeKinds by PrefsBoolean(prefs, "showChangeKinds")
+    override var showSums by PrefsBoolean(prefs, "showSums", true)
+    override var showChangeKinds by PrefsBoolean(prefs, "showChangeKinds", true)
     override var showTimes by PrefsBoolean(prefs, "showTimes", true)
 
     override var longSumPeriodDays by PrefsInt(prefs, "longSumPeriodDays", 30)
