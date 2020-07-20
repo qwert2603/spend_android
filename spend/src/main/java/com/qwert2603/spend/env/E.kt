590eb34aec827c11e138d5b4da181811312b16c2
qwert2603 <qwert2603@mail.ru>
2020-07-20 23:23:07 +0700

    BuildConfig.SERVER_TOKEN

--- spend/src/main/java/com/qwert2603/spend/env/E.kt
+++ spend/src/main/java/com/qwert2603/spend/env/E.kt
@@ -26,25 +26,21 @@ abstract class Env {
                 LogUtils.LogType.ANDROID_ERRORS
             }
 
-    abstract val token: String
+    val token: String = BuildConfig.SERVER_TOKEN
 }
 
 private object ServerTest : Env() {
     override val serverUrl = "http://192.168.1.26:8359"
-    override val token = "41045f4f-05fe-4afe-9dfe-de05c472ae0c"
 }
 
 private object ServerProd : Env() {
     override val serverUrl = "http://192.168.1.26:8354"
-    override val token = "731ee825-619d-4ba3-a09c-dc24b0b5795b"
 }
 
 private object ServerMother : Env() {
     override val serverUrl = "http://192.168.1.26:8354"
-    override val token = "56317ae2-437b-4798-b7da-31657d31b43e"
 }
 
 private object ServerAlexAnya : Env() {
     override val serverUrl = "http://192.168.1.26:8354"
-    override val token = "e25a41b8-5457-4b2e-a8c6-1b5a9d96ef6c"
 }
